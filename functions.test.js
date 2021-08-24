12d63f3577377b535f948e60708c805096b16ae7
blakedyblake <87673203+blakedyblake@users.noreply.github.com>
2021-08-24 14:16:54 -0600

    Descript block and more under the test plan
    A couple instructions I forgot

--- functions.test.js
+++ functions.test.js
@@ -4,17 +4,31 @@ test('Returns 2',()=>{
     expect(returnTwo()).toBe(2);
 } )
 test('greeting returns "Hello, "(name)"',()=>{
-    expect(greeting("Jack")).toEqual('Hello, Jack.')
+    expect(greeting("James")).toEqual('Hello, James.')
+    expect(greeting("Jill")).toEqual('Hello, Jill.')
+
 })
-test('add returns the sum of two numbers',()=>{
-    expect(add(4,7)).toBe(11);
-})
-test('subrtract returns num1 - num2', ()=>{
-    expect(subtract(7,5)).toBe(2)
-})
-test('multiply return two numbers multiplied',()=>{
-    expect(multiply(9,8)).toBe(72)
-})
-test('divide returns num1 /num2',()=>{
-    expect(divide(9,3)).toBe(3)
+
+//Describe block?
+describe('Math functions',()=>{
+    test('add returns the sum of two numbers',()=>{
+        expect(add(4,7)).toBe(11);
+        expect(add(1,2)).toBe(3)
+        expect(add(5,9)).toBe(14)
+    })
+    test('subrtract returns num1 - num2', ()=>{
+        expect(subtract(7,5)).toBe(2)
+        expect(subtract(9,2)).toBe(7)
+    
+    })
+    test('multiply return two numbers multiplied',()=>{
+        expect(multiply(9,8)).toBe(72)
+        expect(multiply(11,4)).toBe(44)
+    
+    })
+    test('divide returns num1 /num2',()=>{
+        expect(divide(9,3)).toBe(3)
+        expect(divide(50,5)).toBe(10)
+    
+    })
 })
